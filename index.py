0cecbd633663886718613d7870d67b5710d1d568
Simon Wallace <simonwallace@users.noreply.github.com>
2017-07-24 18:15:16 +0100

    Fixed registration
    index.py:
    - Updated variable names. These must match those in the Alexa skill intent configuration.
    - Updated the title to "UK Vehicle Enquiry".
    - The registration is now built correctly if 1 or more of the constituent parts are null. Instead of adding "None", it now adds nothing.
    - The vehicle check only occurs if a registration was supplied.

--- index.py
+++ index.py
@@ -2,19 +2,23 @@ from flask import Flask
 from flask_ask import Ask, statement
 from intents.enquiry_intent import EnquiryIntent
 
+TITLE = 'UK Vehicle Enquiry'
+
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 @ask.intent('Enquiry')
-def enquiry(enquiry_type, vehicle, validity, reg_one, reg_two, reg_three,
-            reg_four, reg_five, reg_six, reg_seven):
-    reg = reg_one + reg_two + reg_three + reg_four + reg_five + reg_six + reg_seven
-    intent = EnquiryIntent()
-    result = intent.execute(reg)
-    speech_text = "{} {} {} {} - {} {}".format(enquiry_type, vehicle, validity, reg,
-                                               result['tax']['valid'],
-                                               result['tax']['text'])
-    return statement(speech_text).simple_card('Hello', speech_text)
+def enquiry(subject, vehicle, validity, reg_i, reg_ii, reg_iii, reg_iv, reg_v, reg_vi, reg_vii):
+    reg_list = [ reg_i, reg_ii, reg_iii, reg_iv, reg_v, reg_vi, reg_vii ]
+    reg = ''.join(filter(None, reg_list))
+    speech_text = 'Registration not supplied'
+    if reg:
+        intent = EnquiryIntent()
+        result = intent.execute(reg)
+        speech_text = "{} {} {} {} - {} {}".format(subject, vehicle, validity, reg,
+                                                   result['tax']['valid'],
+                                                   result['tax']['text'])
+    return statement(speech_text).simple_card(TITLE, speech_text)
 
 if __name__ == '__main__':
     app.run()
